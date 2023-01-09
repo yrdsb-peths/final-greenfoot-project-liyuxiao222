b2535fe7aedbc43ecf2d47540cdf8d1d80a034fb
liyuxiao222 <117941566+liyuxiao222@users.noreply.github.com>
2023-01-09 09:44:48 -0500

    Revert "restarted project"

---
+++ XSymbol.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * the image X for tic tac toe
+ * 
+ * @Liyu 
+ * @Dec 2022
+ */
+public class XSymbol extends Actor
+{
+    //resize the shape
+    GreenfootImage X = new GreenfootImage("images/x.png");
+    public XSymbol(){
+        X.scale(190,190);
+        setImage(X);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
