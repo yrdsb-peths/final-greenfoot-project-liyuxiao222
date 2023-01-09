7b162a351c91407790797af9cfec16e22e741b50
liyuxiao222 <117941566+liyuxiao222@users.noreply.github.com>
2023-01-09 09:34:36 -0500

    Revert "Revert "restarted project""

---
+++ BoardPiece.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class boardpeice here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BoardPiece extends Actor
+{   GreenfootImage boardPiece = new GreenfootImage("images/boardpeice.png");
+    public BoardPiece(){
+        boardPiece.scale(145,145);
+        setImage(boardPiece);
+    }
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this)){
+            TicTacToe game = (TicTacToe) getWorld();
+            if(game.getTurn()){
+                game.addObject(new Circle(),getX(), getY());
+                game.board[getX()][getY()] = 1; 
+            }
+        }
+    }
+}
